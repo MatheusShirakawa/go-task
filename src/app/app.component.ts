4a1324f104456a8091e0f88aae62e18b283f11af
MatheusShirakawa <matheus_shirakawa@outlook.com>
2026-01-05 13:46:44 -0300

    :construction: setting up modal service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, Injectable } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { DialogModule } from '@angular/cdk/dialog';
 import { MainContentComponent } from "./components/main-content/main-content.component";
 import { TaskFormModalComponent } from "./components/task-form-modal/task-form-modal.component";
 import { TaskCommentsModalComponent } from "./components/task-comments-modal/task-comments-modal.component";
+import { ModalControllerService } from './services/modal-controller.service';
+// @Injectable({ providedIn: 'platform' })
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, MainContentComponent, TaskFormModalComponent, TaskCommentsModalComponent],
+  imports: [HeaderComponent, MainContentComponent, TaskFormModalComponent, TaskCommentsModalComponent, DialogModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'go-task';
+	title = 'go-task';
 }
---
+++ src/app/services/modal-controller.service.ts
@@ -0,0 +1,43 @@
+import { Injectable, inject } from '@angular/core';
+import { Dialog } from '@angular/cdk/dialog';
+import { TaskFormModalComponent } from '../components/task-form-modal/task-form-modal.component';
+import { TaskCommentsModalComponent } from '../components/task-comments-modal/task-comments-modal.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ModalControllerService {
+	private readonly modalSizeOptions = {
+		maxWidth:'620px',
+		width:'95%',
+	}
+
+	private readonly _dialog = inject(Dialog)
+
+	openNewTaskModal(){
+		return this._dialog.open(TaskFormModalComponent,{
+			...this.modalSizeOptions,
+			data: {
+				mode:'create',
+			}
+		})
+	}
+
+	openEditTaskModal(){
+		return this._dialog.open(TaskFormModalComponent,{
+			...this.modalSizeOptions,
+			data: {
+				mode:'edit',
+			}
+		})
+	}
+
+	openTaskCommentsModal(){
+		return this._dialog.open(TaskCommentsModalComponent,{
+			...this.modalSizeOptions,
+			data: {
+				mode:'create',
+			}
+		})
+	}
+}
